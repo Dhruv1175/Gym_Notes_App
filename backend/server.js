1eb0fe1531b53d5ee8deee19c92344dd4cfd464d
Dhruv1175 <dhruvgajra7@gmail.com>
2025-05-13 22:14:22 +0530

    backend structure

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import connectDB from "./config/db.js";
+import express from "express";
+import dotenv from "dotenv";    
+import cors from "cors";
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/workouts", workoutRoutes);
+app.use("/api/diets", dietRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
